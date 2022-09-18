3ab024def2d60fb7f4801153afb5b97388484276
Casaubon <netzach.yoshida@gmail.com>
2022-09-18 19:40:40 +0900

    feat: 🎸 better design

--- apps/cf-pages/src/pages/about.page.tsx
+++
@@ -1,8 +0,0 @@
-import { BasicLayout } from "@/components/layouts/BasicLayout/BasicLayout";
-import { About } from "@/components/templates/About";
-import { NextPageWithLayout } from "@/lib/next/types";
-
-const Page: NextPageWithLayout = () => <About />;
-Page.getLayout = BasicLayout;
-
-export default Page;
