7c43aa864f4ca3a806f09be57b6d3289ef5cedb4
Casaubon <netzach.yoshida@gmail.com>
2022-09-29 18:02:19 +0900

    feat: 🎸 add storybook

--- apps/cf-pages/.next/static/zJ0VlFZydiQAukibnyJUE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/285-25169a4888d51f15.js","static/css/5aec4facb13927ad.css","static/chunks/pages/index-61eee27d019e1067.js"],"/_error":["static/chunks/pages/_error-6d6f22f5ac783055.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
