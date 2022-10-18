63b902d1242c36b2992e1b73c43a25fb67b3814b
Casaubon <netzach.yoshida@gmail.com>
2022-10-18 12:51:34 +0900

    refactor: 💡 create main component

---
+++ apps/cf-pages/src/components/templates/Top/Top.tsx
@@ -0,0 +1,17 @@
+import { pageTitle } from "@/components/meta";
+import { EmptyRoom } from "@/components/organisms/emptyroom";
+import { Main } from "@/components/organisms/main"
+import Head from "next/head";
+import styles from "./styles.module.css";
+
+export const Top = () => {
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>{pageTitle("トップページ")}</title>
+      </Head>
+      <EmptyRoom />
+      <Main />
+    </main>
+  );
+};
