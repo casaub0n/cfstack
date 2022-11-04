96dc95fc32aea9f8fbfb9a62c92749da76246d6b
Casaubon <netzach.yoshida@gmail.com>
2022-11-04 21:30:47 +0900

    feat: 🎸 add styles

--- apps/cf-pages/src/components/organisms/main/Main.tsx
+++ apps/cf-pages/src/components/organisms/main/Main.tsx
@@ -1,6 +1,5 @@
 import { ArticleSection } from "@/components/molecules/ArticleSection";
 import Image from "next/image";
-import styles from "./styles.module.css";
 
 export const Main = () => {
   return (
