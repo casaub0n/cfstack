105f1c806084603349f8cc2c6deceb174b5be964
Casaubon <netzach.yoshida@gmail.com>
2022-11-17 19:48:52 +0900

    feat: 🎸 change h2 and img postion

--- apps/cf-pages/src/components/organisms/emptyroom/EmptyRoom.tsx
+++ apps/cf-pages/src/components/organisms/emptyroom/EmptyRoom.tsx
@@ -1,17 +1,32 @@
 import { ArticleSection } from "@/components/molecules/ArticleSection";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./styles.module.css"
 
 export const EmptyRoom = () => {
   return (
     <ArticleSection>
       <h2>板橋駅前桜ビル401号室　内見受付中</h2>
-      <Image
+      {/* <Image
         alt="sakura building living room"
         src="sakura-building-livingroom.jpg"
         width={480}
         height={640}
-        layout={"responsive"} />
+        layout={"responsive"} /> */}
+      {/* https://nextjs.org/docs/basic-features/image-optimization */}
+      <div className={styles.img_block}>
+        <Image
+          alt="sakura building living room"
+          src="sakura-building-livingroom.jpg"
+          width={480}
+          height={640}
+          // layout={"responsive"}
+          style={{
+            maxWidth: '100%',
+            height: 'auto'
+          }}
+        />
+      </div>
       <p>板橋駅から徒歩1分の駅近物件です！部屋はフルリニューアル、大きなウオーキングクローゼットとワーキングスペースがあります。</p>
       <p>内見をお待ちしております。</p>
       <ul>
