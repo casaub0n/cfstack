57e3f72d2da67d1c4283b1fbd65adc723861b048
Casaubon <netzach.yoshida@gmail.com>
2022-12-26 17:02:51 +0900

    test: 💍 add a story and a test

---
+++ apps/cf-pages/src/components/atoms/ArticleSection/ArticleSection.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { ArticleSection } from "./ArticleSection";
+
+type T = typeof ArticleSection;
+type Story = ComponentStoryObj<T>;
+
+export default {
+  component: () => (
+      <ArticleSection>
+        <h2>Test</h2>
+        <p>article section</p>
+      </ArticleSection>
+    ),
+} as ComponentMeta<T>;
+
+export const Default: Story = {
+  storyName: "テキスト",
+};
