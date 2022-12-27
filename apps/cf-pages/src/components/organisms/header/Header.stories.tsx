fd217de1d994563de457e9f75ea38d04559ff496
Casaubon <netzach.yoshida@gmail.com>
2022-12-27 18:10:23 +0900

    test: 💍 add a test and format the code

--- apps/cf-pages/src/components/organisms/header/Header.stories.tsx
+++ apps/cf-pages/src/components/organisms/header/Header.stories.tsx
@@ -1,13 +1,13 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
-import { Header } from './Header'
+import { Header } from "./Header";
 
 type T = typeof Header;
-type Story = ComponentStoryObj<T>
+type Story = ComponentStoryObj<T>;
 
 export default {
-  component: () => <Header />
-} as ComponentMeta<T>
+  component: () => <Header />,
+} as ComponentMeta<T>;
 
 export const Default: Story = {
-  storyName: "Header"
+  storyName: "Header",
 };
