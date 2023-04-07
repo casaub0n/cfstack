f27365607e21993be900f4f00861db188592506c
Casaubon <netzach.yoshida@gmail.com>
2023-04-08 00:14:35 +0900

    refactor: 💡 tsconfig

--- apps/cf-pages/next.config.js
+++ apps/cf-pages/next.config.js
@@ -1,12 +1,14 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true, // https://nextjs.org/docs/messages/swc-disabled
+  },
   swcMinify: true,
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "api.ts"],
   images: {
-    loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/dycpos4uc/image/upload'
-  }
-}
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/dycpos4uc/image/upload",
+  },
+};
 module.exports = nextConfig;
