2b52cb6b3dd2d39e83bee7b11320f0417a35917c
Casaubon <netzach.yoshida@gmail.com>
2023-04-20 00:58:22 +0900

    feat: 🎸 monorepo storybook

---
+++ apps/catalog/.storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: [
+    {
+      directory: "../../app1/src",
+      titlePrefix: "app1",
+    },
+    {
+      directory: "../../app2/src",
+      titlePrefix: "app2",
+    },
+    {
+      directory: "../../../packages/core/src",
+      titlePrefix: "core",
+    },
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react-vite",
+  docs: {
+    autodocs: true,
+  },
+};
