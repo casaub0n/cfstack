d878714fd093f3d9a85b8ffaee2b9d2eb94fe1e2
Casaubon <netzach.yoshida@gmail.com>
2023-04-28 01:40:22 +0900

    feat: 🎸 webpack config

--- apps/catalog/.storybook/main.js
+++ apps/catalog/.storybook/main.js
@@ -1,16 +1,10 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     {
-      directory: "../../app1/src",
-      titlePrefix: "app1",
-    },
-    {
-      directory: "../../app2/src",
-      titlePrefix: "app2",
-    },
-    {
-      directory: "../../../packages/core/src",
-      titlePrefix: "core",
+      directory: "../../cf-pages/src/",
+      titlePrefix: "cf-pages",
     },
   ],
   addons: [
@@ -18,8 +12,24 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react-vite",
+  framework: "@storybook/react-webpack5",
+  core: {
+    disableTelemetry: true,
+  },
   docs: {
     autodocs: true,
+  },
+  features: {
+    babelModeV7: true,
+  },
+  babel: (config) => {
+    return { ...config, rootMode: "upward" };
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    return config;
   },
 };
