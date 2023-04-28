5408f77b4fa66d17d5a7477a6d326d381be95726
Casaubon <netzach.yoshida@gmail.com>
2023-04-28 18:28:43 +0900

    refactor: 💡 remove storybook in cf-pages

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,21 +1,13 @@
 module.exports = {
-  env: {
-    "jest/globals": true,
-    es6: true,
+  extends: ["next", "turbo", "prettier"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-  plugins: ["jest"],
-  extends: [
-    "eslint:recommended",
-    "next",
-    "turbo",
-    "prettier",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-  ],
-  rules: {
-    "jest/expect-expect": "off",
-    "no-unused-vars": "off",
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
   },
 };
