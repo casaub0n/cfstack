a91f4356852b94271d47038154c078528ba8c5ab
Casaubon <netzach.yoshida@gmail.com>
2023-05-10 18:49:13 +0900

    refactor: 💡 TypeScript paths
    change default to @/: ./src/*

--- apps/cf-pages/src/pages/index.page.tsx
+++ apps/cf-pages/src/pages/index.page.tsx
@@ -1,6 +1,6 @@
-import { NextPageWithLayout } from "../types/next/type";
-import { Layout } from "../components/layouts";
-import { Top } from "../components/templates/Top";
+import { NextPageWithLayout } from "@/types/next/type";
+import { Layout } from "@/components/layouts";
+import { Top } from "@/components/templates/Top";
 
 const IndexPage: NextPageWithLayout = () => <Top />;
 
