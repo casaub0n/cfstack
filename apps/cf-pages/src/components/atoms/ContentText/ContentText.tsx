9d9dae3a6c13e0cb31e984e3087f8e09d7737c00
Casaubon <netzach.yoshida@gmail.com>
2023-06-13 21:22:21 +0900

    refactor: 💡 type import

--- apps/cf-pages/src/components/atoms/ContentText/ContentText.tsx
+++ apps/cf-pages/src/components/atoms/ContentText/ContentText.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from "react";
+import type { ComponentPropsWithoutRef } from "react";
 import type { FC } from "react";
 
 import clsx from "clsx";
--- apps/cf-pages/src/pages/index.page.tsx
+++ apps/cf-pages/src/pages/index.page.tsx
@@ -2,7 +2,7 @@
 import { Layout } from "@/components/layouts";
 import { Top } from "@/components/templates/Top";
 
-import { NextPageWithLayout } from "@/types/next/type";
+import type { NextPageWithLayout } from "@/types/next/type";
 
 const IndexPage: NextPageWithLayout = () => <Top />;
 
