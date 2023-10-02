55a2cf6df5e58a165c582627ad09ab57fc4c2410
casaub0n <netzach.yoshida@gmail.com>
2023-10-02 17:17:07 +0900

    feat: 🎸 structure data

--- apps/cf-pages/src/components/layouts/json-ld.ts
+++ apps/cf-pages/src/components/layouts/json-ld.ts
@@ -1,3 +1,7 @@
+/**
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata#json-ld
+ * @see https://developers.google.com/search/docs/appearance/structured-data/intro-structured-data?hl=ja
+ */
 export const jsonLd = {
   "@context": "https://schema.org",
   "@type": "Organization",
--- apps/cf-pages/src/components/templates/Top/Top.tsx
+++ apps/cf-pages/src/components/templates/Top/Top.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
+import { jsonLd } from "@/components/layouts/json-ld";
 import { pageTitle } from "@/components/meta";
 import { Main } from "@/components/molecules/main";
 
@@ -9,6 +10,10 @@ export const Top = () => {
   return (
     <main className={styles.content}>
       <Head>
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <title>{pageTitle("トップページ")}</title>
       </Head>
       <Main />
