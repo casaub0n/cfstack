0819759879b74f6b55406093920980e40aff7708
casaub0n <netzach.yoshida@gmail.com>
2023-10-14 17:11:00 +0900

    fix: 🐛 next.js config

--- apps/cf-pages/src/components/layouts/Layout.tsx
+++ apps/cf-pages/src/components/layouts/Layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactElement } from "react";
 
 import Head from "next/head";
-import Script from "next/script";
 
 import { Footer } from "@/components/organisms/footer";
 import { Header } from "@/components/organisms/header";
@@ -27,11 +26,11 @@ export const Layout = (page: ReactElement) => {
         <div className={styles.content}>{page}</div>
         <Footer />
       </div>
-      <Script
+      {/* <Script
         id='json-ld'
         type='application/ld+json'
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
+      /> */}
     </>
   );
 };
