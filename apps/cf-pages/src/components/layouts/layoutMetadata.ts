a7017af53ac1289270327107d283383d051e431a
casaub0n <netzach.yoshida@gmail.com>
2023-10-17 18:36:03 +0900

    feat: 🎸 seo

--- apps/cf-pages/src/components/layouts/layoutMetadata.ts
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from "next";
-
-export const layoutMetadata: Metadata = {
-  title: "（有）エーワンハウジング吉田",
-  description:
-    "（有）エーワンハウジング吉田は不動産の賃貸・管理・仲介業・火災保険代理店を営んでいます。",
-};
